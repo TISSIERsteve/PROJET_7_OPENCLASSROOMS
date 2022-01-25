70ad0bb8438d8fe1de35c535891e94968b3af3e9
Tissier Steve <stevetissier@orange.fr>
2022-01-25 12:48:43 +0100

    first commit

--- frontend/src/Screens/ProfileScreen.js
+++ frontend/src/Screens/ProfileScreen.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+// CSS
+import "../Styles.css/Screens.css/ProfileScreen.css"
+
 function ProfileScreen() {
-    return <div>Page profil</div>;
+
+    const open = () => {
+        document.getElementById("open").classList.toggle("active")
+    }
+
+    return <div>
+        <button onClick={open}
+            type="button"
+            className="open-sidebar button_sidebar">
+            <i className="fa fa-bars" />
+        </button>
+        <div id='open' className="aside_form">
+            <aside className="aside">
+                <ul>
+                    <li className='aside_li'>Déconnection<i class="fas fa-power-off"></i></li>
+                </ul>
+            </aside>
+        </div>
+        Page profil</div>;
 }
 
 export default ProfileScreen;
